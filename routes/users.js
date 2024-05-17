795309e76d0ec3c81dcc4b427d2cb28f2ab264c8
abaez8031 <abaez8031@gmail.com>
2024-05-17 01:26:44 -0400

    redirect middleware

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,22 @@ const router = express.Router();
 const passport = require("passport");
 const wrapAsync = require("../utils/wrapAsync");
 const User = require("../models/user");
+const { storeReturnTo } = require('../middleware');
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", wrapAsync(async (req, res) => {
+router.post("/register", wrapAsync(async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
-    req.flash("success", "Welcome to Yelp Camp!");
-    res.redirect("/campgrounds")
+    req.login(registeredUser, (err) => {
+      if(err) return next(err);
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds")
+    })
   } catch(e) {
     req.flash("error", e.message);
     res.redirect("register")
@@ -25,9 +29,11 @@ router.get("/login", (req,res) => {
   res.render("users/login")
 });
 
-router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login"}), async(req,res) => {
-  req.flash("success", "welcome back!");
-  res.redirect("/campgrounds")
+router.post("/login", storeReturnTo, passport.authenticate("local", { failureFlash: true, failureRedirect: "/login"}), (req,res) => {
+  req.flash("success", "Welcome Back!");
+  const redirectUrl = res.locals.returnTo || "/campgrounds";
+  delete req.session.returnTo;
+  res.redirect(redirectUrl)
 });
 
 router.get("/logout", (req,res) => {
