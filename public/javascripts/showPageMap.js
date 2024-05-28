8ca5fdc0ddce3331ac55fefa78a91397dab60103
abaez8031 <abaez8031@gmail.com>
2024-05-28 02:26:42 -0400

    styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,20 +1,20 @@
-const parsedCampground = JSON.parse(campground)
-
 mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9',
+        style: 'mapbox://styles/mapbox/light-v10',
         zoom: 4,
-        center: parsedCampground.geometry.coordinates
+        center: campground.geometry.coordinates
         // center: [40,40]
     });
 
+    map.addControl(new mapboxgl.NavigationControl());
+
     new mapboxgl.Marker()
-      .setLngLat(parsedCampground.geometry.coordinates)
+      .setLngLat(campground.geometry.coordinates)
       .setPopup(
         new mapboxgl.Popup({offset: 25})
         .setHTML(
-          `<h3>${parsedCampground.title}</h3><p>${parsedCampground.location}</p>`
+          `<h3>${campground.title}</h3><p>${campground.location}</p>`
         )
       )
       .addTo(map);
